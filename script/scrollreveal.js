e744bd8736b4d0438b2ede202ce8b14715b36f94
Victor Escalona <victorescalonag@gmail.com>
2019-01-26 18:39:32 -0400

    Se añadio scrollreveal a todo

---
+++ script/scrollreveal.js
@@ -0,0 +1,65 @@
+window.sr = ScrollReveal();
+sr.reveal('.navbar', {
+    duration:1000,
+    origin:'top',
+    distance:'300px'
+});
+sr.reveal('#container-slider-services', {
+    duration:1000,
+    origin:'bottom',
+    distance:'300px'
+});
+sr.reveal('.carousel', {
+    duration:1000,
+    origin:'bottom',
+    distance:'300px'
+});
+sr.reveal('.col-grande', {
+    duration:1000,
+    origin:'left',
+    distance:'300px'
+});
+sr.reveal('#container-services-box-index', {
+    duration:1000,
+    origin:'left',
+    distance:'300px'
+});
+
+/*Services*/
+sr.reveal('#banner-services-page', {
+    duration:1000,
+    origin:'left',
+    distance:'300px'
+});
+sr.reveal('#container-services-total', {
+    duration:1000,
+    origin:'bottom',
+    distance:'300px'
+});
+/*Soat*/
+sr.reveal('#banner-soat-page', {
+    duration:1000,
+    origin:'left',
+    distance:'300px'
+});
+sr.reveal('#container-soat', {
+    duration:1000,
+    origin:'right',
+    distance:'300px'
+});
+/*Contactos */
+sr.reveal('.bg-ubicacion', {
+    duration:1000,
+    origin:'left',
+    distance:'300px'
+});
+sr.reveal('.bg-llamanos', {
+    duration:1000,
+    origin:'right',
+    distance:'300px'
+});
+sr.reveal('.bg-correo', {
+    duration:1000,
+    origin:'bottom',
+    distance:'300px'
+});
